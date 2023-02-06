aeffaa9d17d8e5e35aabbf57fda57478df7250d4
Ilia.Levin <krenkbaus.99@mail.ru>
2023-02-06 22:00:45 +0300

    taking source code

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,81 +1,68 @@
-import React, { useState } from 'react'
-import api from '../api'
+import React, { useState } from "react";
+import api from "../api"
 
 const Users = () => {
-  const [users, setUsers] = useState(api.users.fetchAll())
+  const [users, setUsers] = useState(api.users.fetchAll());
 
   const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== userId))
-  }
-
-  // const renderPeople = () => {
-  //   return (users.map((user) => (
-  //     <tr key={user._id}>
-  //       <td>{user.name}</td>
-  //       <td>{user.qualities.map((q) => {
-  //         return <span key={q._id}
-  //           className={`badge bg-${q.color}`}>{q.name} </span>
-  //       })}</td>
-  //       <td>{user.profession.name}</td>
-  //       <td>{user.completedMeetings}</td>
-  //       <td>{user.rate}</td>
-  //       <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>Удалить</button></td>
-  //     </tr>
-  //   )))
-  // }
+    setUsers(users.filter((user) => user._id !== userId));
+  };
 
   const renderPhrase = (number) => {
-    if (number) {
-      let z = number % 100 / 10
-      let x = number % 10
-      z >= 1.1 && z <= 1.4 ? number += ' человек' :
-        x === 2 || x === 3 || x === 4 ? number += ' человека' :
-          number += ' человек'
-      return <h1><span className='badge bg-primary'>{number} тусанёт с тобой сегодня</span></h1>
-    } else return <h1><span className='badge bg-danger'>Никто с тобой не тусанёт</span></h1>
-  }
-
-  if (users.length === 0) {
-    return renderPhrase(users.length)
-  }
+    const lastOne = Number(number.toString().slice(-1));
+    if (number > 4 && number < 15) return "человек тусанет"
+    if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут"
+    if (lastOne === 1) return "человек тусанет"
+    return "человек тусанет"
+  };
 
   return (
     <>
-      {renderPhrase(users.length)}
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился,раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {(users.map((user) => (
-            <tr key={user._id}>
-              <td>{user.name}</td>
-              <td>{user.qualities.map((q) => {
-                return <span key={q._id}
-                  className={`badge bg-${q.color}`}>{q.name} </span>
-              })}</td>
-              <td>{user.profession.name}</td>
-              <td>{user.completedMeetings}</td>
-              <td>{user.rate}</td>
-              <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>Удалить</button></td>
+      <h2>
+        <span
+          className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}>
+          {users.length > 0 ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня` : "Никто с тобой не тусанет"}
+        </span>
+      </h2>
+      {users.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
             </tr>
-          )))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((item) => (
+                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                      {item.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate} /5</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-danger">
+                    Удалить
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
-
-
-
-
-
-
 export default Users
